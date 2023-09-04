c5d69196583549e312e55ed7117a70d40bf4be5e
Mafmudin <>
2023-09-04 15:28:53 +0700

    update (not finished)

--- list_comprehesions.py
+++ list_comprehesions.py
@@ -1,1 +1,22 @@
-from itertools import permutations
+def permutation(x,y,z,n):
+	hasil = []
+	for x in range(0, n+1):
+		for y in range(0, n+1):
+			for z in range(0, n+1):
+				if x <= y <= z and x+y+z <=n:
+					hasil.append((x,y,z))
+	return hasil
+	
+def main():
+	x = int(input())
+	y = int(input())
+	z = int(input())
+	n = int(input())
+
+	permutations = permutation(x,y,z,n)
+
+	for permutation in permutations:
+		print(permutation)
+
+
+main()
