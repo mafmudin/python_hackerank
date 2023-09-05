bbad370282c85704f8b6a3cef43b2026a04884a9
Mafmudin <>
2023-09-05 10:06:38 +0700

    finding the precentage

---
+++ finding_the_precentage.py
@@ -0,0 +1,15 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+
+sum = 0
+for i in student_marks[query_name]:
+    sum += i
+
+avg = sum / len(student_marks[query_name])
+print(f"{avg:.2f}")
+        
